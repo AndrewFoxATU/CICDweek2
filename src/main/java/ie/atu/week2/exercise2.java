a75c34257db375546c7d91114fb1284b532c3814
AndrewFoxATU <G00417373@atu.ie>
2024-09-25 15:02:53 +0100

    Exercise 2

--- src/main/java/ie/atu/week2/exercise2.java
+++ src/main/java/ie/atu/week2/exercise2.java
@@ -2,13 +2,12 @@ package ie.atu.week2;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class RequestController {
-    @GetMapping("/Hello")
-    public String Hello() {
-        return "Hi there";
+public class exercise2 {
+    @GetMapping("/user/{id}")
+    public String getUser(@PathVariable long id) {
+        return "User ID: " + id;
     }
 }
